586f74236878e47b2e14a7c41e0795b3477a4a9d
Ashish Singh <ashish.singh@uphs.upenn.edu>
2020-04-14 13:18:03 -0400

    adding missing header file

--- ReorientBvecTest/src/mainwindow.h
+++ ReorientBvecTest/src/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include "itkCSVArray2DFileReader.h"
+
 namespace Ui {
 class MainWindow;
 }
@@ -12,8 +14,14 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+	typedef itk::CSVArray2DFileReader<double> CSVFileReaderType;
+	typedef vnl_matrix<double> MatrixType;
+	
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+	void WriteCSVFiles(MatrixType matrix, QString filename);
+	MatrixType ReadBVecFile(QString filename);
 
 public slots:
     void OnOpenFileButtonClicked();
